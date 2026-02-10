99121ef29f21c134f88d07841975e5ed3f501fd2
mirkogulin2001 <mirkogulin2001@gmail.com>
2026-02-10 10:55:24 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,10 @@
 import bcrypt
 
 def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-def check_password(password, hashed):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
+def check_password(password, hashed_password):
+    try:
+        return bcrypt.checkpw(password.encode(), hashed_password.encode())
+    except:
+        return False
